1f25c396e3b3069dabc3cec1b87a8ec003774fe0
unknown <jalandete@unal.edu.co>
2025-12-01 20:10:55 -0500

    HotFixes in Match.py

--- match.py
+++ match.py
@@ -7,12 +7,12 @@ import dao
 
 def like_user(db: Session, sender_user_fk: int, liked_user_fk: int, link_date: Optional[datetime] = None):
     
-    dao.create_like(Session, sender_user_fk, liked_user_fk, link_date)
+    like = dao.create_like(Session, sender_user_fk, liked_user_fk, link_date)
 
     if check_like_exists(db, liked_user_fk, sender_user_fk):
         create_match(db, sender_user_fk, liked_user_fk) 
 
-    return 
+    return like is None
 
 def check_like_exists(db: Session, sender_user_fk: int, liked_user_fk: int) -> bool:
     like = dao.find_like(db, sender_user_fk, liked_user_fk)
@@ -29,7 +29,7 @@ def break_match(db: Session, user1_fk: int, user2_fk: int):
     relacionship = dao.get_couple_relationship_between_users(db, user1_fk, user2_fk)
 
     if relacionship:
-        dao.update_relationship_state(db, relacionship.state_fk, "broken")
-        return True
+        state = dao.update_relationship_state(db, relacionship.state_fk, "broken")
+        return state is not None
     
     return False
